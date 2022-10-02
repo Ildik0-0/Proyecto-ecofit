20052f550850243159602677570e1996a5b2325b
Ildik0-0 <iremar.rivas@gmail.com>
2022-10-01 21:46:19 -0300

    Se conecto la DB

---
+++ src/keys.js
@@ -0,0 +1,10 @@
+//direccion de la base de datos
+module.exports ={
+
+    database: {
+        host: 'localhost:3308',
+        user: 'root',
+        password:'admin',
+        database: 'ecofit',
+    }
+};
